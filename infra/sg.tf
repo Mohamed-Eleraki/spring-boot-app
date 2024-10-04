04b68e425d494643becf1e9e39a552c34a3aa63f
mohamed eleraki <mohamed-ibrahim2021@outlook.com>
2024-10-04 10:55:50 +0300

    append ssh port

--- infra/sg.tf
+++ infra/sg.tf
@@ -8,17 +8,8 @@ resource "aws_security_group" "secgrp_RDS_01" {
 
 resource "aws_vpc_security_group_ingress_rule" "secgrp_RDS_01_ingress-01" {
   security_group_id = aws_security_group.secgrp_RDS_01.id
-  #cidr_ipv4 = "0.0.0.0/0"
-  cidr_ipv4   = "${aws_instance.bastion_host_01.private_ip}/32"
-  from_port   = 5432
-  to_port     = 5432
-  ip_protocol = "tcp"
-}
-
-resource "aws_vpc_security_group_ingress_rule" "secgrp_RDS_01_ingress_02" {
-  security_group_id = aws_security_group.secgrp_RDS_01.id
-  #cidr_ipv4 = "0.0.0.0/0"
-  cidr_ipv4   = "${aws_instance.bastion_host_02.private_ip}/32"
+  cidr_ipv4 = "10.0.0.0/16"
+  #cidr_ipv4   = "${aws_instance.bastion_host_01.private_ip}/32"
   from_port   = 5432
   to_port     = 5432
   ip_protocol = "tcp"
