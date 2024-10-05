86d122212f276e0b76e6250f9bc0a7e03857fce2
mohamed eleraki <mohamed-ibrahim2021@outlook.com>
2024-10-05 14:06:28 +0300

    disable health check

--- infra/asg.tf
+++ infra/asg.tf
@@ -61,8 +61,8 @@ resource "aws_autoscaling_group" "web_asg" {
   desired_capacity     = 2
   max_size             = 2
   min_size             = 1
-  health_check_type    = "ELB"
-  health_check_grace_period = 600
+  #health_check_type    = "ELB"
+  #health_check_grace_period = 600
   launch_template {
     id      = aws_launch_template.web_launch_template.id
     version = "$Latest"
